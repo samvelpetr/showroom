c39674911e36d4fb4cd5f20c02fd33c9466c9071
samvelpetr <petrosans05@gmail.com>
2023-09-09 00:52:23 +0400

    12222

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ const swiper8 = new Swiper('.furniture__namesSwiper', {
     spaceBetween: 20,
     slidesPerView: 2.5
 })
+
 const swiper6 = new Swiper('.swiperFurniture2', {
     freeMode: true,
     spaceBetween: 25,
@@ -181,15 +182,15 @@ const burger__menuIcon = document.querySelector('.burger__menuIcon')
 burger__menuIcon.addEventListener('click', function () {
     burgerMenu1.classList.add('displayBlock')
 })
-const hoverIcon = [...document.querySelectorAll('.hoverIcon')].splice(0,3)
+const hoverIcon = [...document.querySelectorAll('.hoverIcon')].splice(0, 3)
 const hoverIcon1 = [...document.querySelectorAll('.hoverIcon')].splice(3)
-const modern__hoverItems = [...document.querySelectorAll('.modern__hoverItems')].splice(0,3)
+const modern__hoverItems = [...document.querySelectorAll('.modern__hoverItems')].splice(0, 3)
 const modern__hoverItems1 = [...document.querySelectorAll('.modern__hoverItems')].splice(3)
 
 // hoverIcon.forEach((e,i)=>{
 //     try {
-        
-    
+
+
 //     e.addEventListener('mouseover',function(){
 //         console.log(true);
 //         modern__hoverItems[i].classList.add('displayBlock')
@@ -197,32 +198,38 @@ const modern__hoverItems1 = [...document.querySelectorAll('.modern__hoverItems')
 //     e.addEventListener('mouseout',function(){
 //         modern__hoverItems[i].classList.remove('displayBlock')
 //     })}catch (error) {
-        
+
 //     }
 // })
-hoverIcon1.forEach((e,i)=>{
-    e.addEventListener('click',function(){
+hoverIcon1.forEach((e, i) => {
+    e.addEventListener('click', function () {
         modern__hoverItems1[i].classList.add('displayBlock')
 
     })
 })
 document.body.onclick = function (e) {
-    if (e.target.localName == 'svg' || e.target.className == 'burger__menuIcon') {
-        return 0
+
+    try {
+        if (e.target.localName == 'svg' || e.target.className == 'burger__menuIcon') {
+            return 0
+
+        }
+
+
+        else {
+            burgerMenu1.classList.remove('displayBlock')
+
+        }
+        if (e.target.className != 'hoverIcon' || e.target.className != 'modern__hoverItems') {
+
+            modern__hoverItems1[0].classList.remove('displayBlock')
+            modern__hoverItems1[1].classList.remove('displayBlock')
+            modern__hoverItems1[2].classList.remove('displayBlock')
+        }
+    } catch (error) {
 
     }
-    
 
-    else {
-        burgerMenu1.classList.remove('displayBlock')
-
-    }
-    if( e.target.className !='hoverIcon' || e.target.className !='modern__hoverItems'){
-
-        modern__hoverItems1[0].classList.remove('displayBlock')
-        modern__hoverItems1[1].classList.remove('displayBlock')
-        modern__hoverItems1[2].classList.remove('displayBlock')
-    }
 }
 popUpBut2.addEventListener('click', function () {
     popUp1.classList.add('displayFlex')
@@ -232,10 +239,15 @@ popup1But.addEventListener('click', function () {
     popUp1.classList.add('displayFlex')
     document.body.classList.add('overflow__hiden')
 })
-popUp1Close.addEventListener('click', function () {
-    popUp1.classList.remove('displayFlex')
-    document.body.classList.remove('overflow__hiden')
-})
+try {
+
+    popUp1Close.addEventListener('click', function () {
+        popUp1.classList.remove('displayFlex')
+        document.body.classList.remove('overflow__hiden')
+    })
+} catch (error) {
+
+}
 const catalog__mobile = document.querySelector('.catalog__mobile')
 const catalog__780 = document.querySelector('.catalog__780')
 const catalog__360 = document.querySelector('.catalog__360')
@@ -243,32 +255,32 @@ const hotelNamesCat = [...document.querySelectorAll('.catalog__780 .hotel__names
 const hotelBacks = [...document.querySelectorAll('.catalog__open_back_hotel')]
 const close__catalog780 = document.querySelector('.close__catalog780')
 const close__catalog360 = document.querySelector('.close__catalog360')
-const hotelBlocks = [...document.querySelectorAll('.catalog__open_hotel')]
+const hotelBlocks = [...document.querySelectorAll('.catalog__open_hotels')]
 const hotelBlocks1 = [...document.querySelectorAll('.catalog__360 .catalog__open_hotels')]
 const hotelNamesCat1 = [...document.querySelectorAll('.catalog__360_mobile2   .hotel__names__main li')]
 
 const hotelBacks1 = [...document.querySelectorAll('.catalog__360 .catalog__open_back_hotel')]
 const catalog__360_open = document.querySelector('.catalog__360_open')
-const  catalog__360_mobile2 =document.querySelector('.catalog__360_mobile2')
-const takeBackCall1= document.querySelector('.catalog__360_mobile1 div span')
-const header__block2svg =document.querySelector('.header__block2  .input__svg')
-header__block2svg.onclick= function(){
+const catalog__360_mobile2 = document.querySelector('.catalog__360_mobile2')
+const takeBackCall1 = document.querySelector('.catalog__360_mobile1 div span')
+const header__block2svg = document.querySelector('.header__block2  .input__svg')
+header__block2svg.onclick = function () {
     catalog__360.classList.add('displayBlock')
 }
-takeBackCall1.onclick= function () {
+takeBackCall1.onclick = function () {
     popUp1.classList.add('displayFlex')
 }
-catalog__360_open.onclick =function () {
+catalog__360_open.onclick = function () {
     catalog__360_mobile2.classList.add('displayBlock')
 }
 
 
 const catalog_mobileBackcall = document.querySelector('.catalog__360_mobile1 > div span')
 
-catalog_mobileBackcall.onclick= function () {
+catalog_mobileBackcall.onclick = function () {
     popUp1.classList.add('displayFlex')
 }
-burger__menuIcon.onclick =function () {
+burger__menuIcon.onclick = function () {
     catalog__360.classList.add('displayBlock')
 }
 catalog__mobile.onclick = function () {
@@ -276,7 +288,7 @@ catalog__mobile.onclick = function () {
 }
 close__catalog780.onclick = function () {
     catalog__780.classList.remove('displayBlock')
-    
+
 }
 close__catalog360.onclick = function () {
     catalog__360_mobile2.classList.remove('displayBlock')
@@ -284,23 +296,32 @@ close__catalog360.onclick = function () {
 
 }
 console.log(hotelNamesCat1);
-hotelNamesCat1.forEach((e, i) => {
-    hotelBacks1[i].addEventListener('click', function () {
-        hotelBlocks1[i].classList.remove('displayBlock')
+try {
 
-    })
-    e.addEventListener('click', function () {
-        hotelBlocks1[i].classList.add('displayBlock')
-    })
-})
+    hotelNamesCat1.forEach((e, i) => {
+        hotelBacks1[i].addEventListener('click', function () {
+            hotelBlocks1[i].classList.remove('displayBlock')
 
-
-hotelNamesCat.forEach((e, i) => {
-    hotelBacks[i].addEventListener('click', function () {
-        hotelBlocks[i].classList.remove('displayBlock')
-
+        })
+        e.addEventListener('click', function () {
+            hotelBlocks1[i].classList.add('displayBlock')
+        })
     })
-    e.addEventListener('click', function () {
-        hotelBlocks[i].classList.add('displayBlock')
+} catch (error) {
+
+}
+
+try {
+
+    hotelNamesCat.forEach((e, i) => {
+        hotelBacks[i].addEventListener('click', function () {
+            hotelBlocks[i].classList.remove('displayBlock')
+
+        })
+        e.addEventListener('click', function () {
+            hotelBlocks[i].classList.add('displayBlock')
+        })
     })
-})
+} catch (error) {
+
+}
